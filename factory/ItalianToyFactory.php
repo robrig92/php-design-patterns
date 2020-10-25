83f336f9d5548f8c324516d5ac4e20dfe85c3227
Roberto Padilla <robert.ipo@hotmail.com>
2020-10-24 20:50:52 -0500

    Solved bugs

--- factory/ItalianToyFactory.php
+++ factory/ItalianToyFactory.php
@@ -1,8 +1,8 @@
 <?php
 
-require_once(__DIR__ . './models/Toy.php');
-require_once(__DIR__ . './models/ItalianToy.php');
-require_once(__DIR__ . './ToyFactory.php');
+require_once(__DIR__ . '/models/Toy.php');
+require_once(__DIR__ . '/models/ItalianToy.php');
+require_once(__DIR__ . '/ToyFactory.php');
 
 class ItalianToyFactory extends ToyFactory
 {
