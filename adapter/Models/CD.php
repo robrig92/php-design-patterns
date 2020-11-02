8ec37413a8901c1e839977b401fff180228cafa2
Roberto Padilla <robert.ipo@hotmail.com>
2020-11-01 20:14:50 -0600

    removed tags and creating adapter example

---
+++ adapter/Models/CD.php
@@ -0,0 +1,35 @@
+<?php
+
+class CD
+{
+    private $name;
+    private $duration;
+    private $songs;
+
+    public function __construct(string $name, float $duration, array $songs)
+    {
+        $this->name = $name;
+        $this->songs =$songs;
+        $this->duration = $duration;
+    }
+
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    public function getDuration()
+    {
+        return $this->duration;
+    }
+
+    public function getSongs()
+    {
+        return $this->songs;
+    }
+
+    public function play(int $number)
+    {
+        renderln("Playing {$this->songs[$number]}");
+    }
+}
