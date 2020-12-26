2341141863166966a930ff818a89a7f7b7c93850
Roberto Padilla <robert.ipo@hotmail.com>
2020-12-26 16:00:42 -0600

    added another guild to the example

--- iterator/Game.php
+++ iterator/Game.php
@@ -9,24 +9,46 @@ class Game implements ApplicationInterface
 {
     public function execute(): void
     {
-        $dragonsOfHeaven = new Guild('Dragons of Heaven');
-
-        $dragonsOfHeaven->addMember(new GameCharacter('Tuna', 99, 'Knight'));
-        $dragonsOfHeaven->addMember(new GameCharacter('Saitama', 99, 'Hero'));
-        $dragonsOfHeaven->addMember(new GameCharacter('Krionoah', 99, 'Orc'));
-
+        $dragonsOfHeaven = $this->craftDragonsOfHeavenGuild();
         $this->showGuildMembers($dragonsOfHeaven);
+
+        $lans = $this->craftLansGuild();
+        $this->showGuildMembers($lans);
     }
 
     protected function showGuildMembers($guild)
     {
         $iterator = $guild->createIterator();
 
-        renderln($guild->getName());
+        renderln("Guild " . $guild->getName());
 
         while ($iterator->hasNext()) {
             $member = $iterator->next();
             renderln($member->getName());
         }
+
+        renderln();
+    }
+
+    protected function craftDragonsOfHeavenGuild()
+    {
+        $dragonsOfHeaven = new Guild('Dragons of Heaven');
+
+        $dragonsOfHeaven->addMember(new GameCharacter('Tuna', 99, 'Knight'));
+        $dragonsOfHeaven->addMember(new GameCharacter('Saitama', 99, 'Hero'));
+        $dragonsOfHeaven->addMember(new GameCharacter('Krionoah', 99, 'Orc'));
+
+        return $dragonsOfHeaven;
+    }
+
+    protected function craftLansGuild()
+    {
+        $lans = new Guild('LaNs');
+
+        $lans->addMember(new GameCharacter('Mayu', 99, 'Lord Knight'));
+        $lans->addMember(new GameCharacter('Meme', 99, 'Priest'));
+        $lans->addMember(new GameCharacter('Chompona', 99, 'Champ'));
+
+        return $lans;
     }
 }
